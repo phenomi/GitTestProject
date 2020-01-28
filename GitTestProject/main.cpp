befc6137b77d2040232b7d9323b54b374e346350
Olavi Kurola <olavi.kurola@tuni.fi>
2020-01-29 00:32:10 +0200

    Added switch-case statements for different kind of operations in main.cpp

--- GitTestProject/main.cpp
+++ GitTestProject/main.cpp
@@ -9,11 +9,35 @@ int main() {
 
 	int number1 = 1;
 	int number2 = 1;
+	char chosenOperator;
 	cout << "Please enter two (2) integers that you want" << endl
 		<< "to add, subtract, divide, or multiply." << endl << endl;
 
 	cout << "Entered integers: ";
 	cin >> number1 >> number2;
+
+	cout << "Please type + , - , / , or * depending on" << endl
+		<< "what kind of an operation you want to perform." << endl << endl;
+
+	cout << "Desired operation: ";
+	cin >> chosenOperator;
+
+	switch(chosenOperator) {
+		case '+':
+			addNumbers(number1, number2);
+			break;
+		case '-':
+			subtractNumbers(number1, number2);
+			break;
+		case '/':
+			divideNumbers(number1, number2);
+			break;
+		case '*':
+			multiplyNumbers(number1, number2);
+			break;
+		default:
+			cout << "Invalid input." << endl;
+	}
 
 
 	return 0;
