a059a1820fd02cc74c17d84a3ae7f023380cd4e0
Olavi Kurola <olavi.kurola@tuni.fi>
2020-01-29 00:17:20 +0200

    Added function definitions for adding and subtracting numbers in main.cpp

--- GitTestProject/main.cpp
+++ GitTestProject/main.cpp
@@ -3,11 +3,22 @@
 using namespace std;
 
 int addNumbers(int number1, int number2);
-int subtractNumbers(int nubmer1, int number2);
+int subtractNumbers(int number1, int number2);
 
 int main() {
 
 
 
 	return 0;
+}
+
+
+int addNumbers(int number1, int number2)
+{
+	return number1 + number2;
+}
+
+int subtractNumbers(int number1, int number2)
+{
+	return number1 - number2;
 }
